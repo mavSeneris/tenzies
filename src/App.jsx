93821e100fdf484cf37178eac6e8c16dee04a8a0
Maverick Seneris <mavseneris@gmail.com>
2022-10-11 05:58:40 +0800

    added styles to App-main element

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      
+      <main>
+        <h1>Tenzies</h1>
+        <p>Roll until all the dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </main>
     </div>
   )
 }
